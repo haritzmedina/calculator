0f00695b093f66b2095a091cc240e74a8f33de90
Haritz Medina <haritzmedina@gmail.com>
2016-04-19 16:43:19 +0200

    Added new tests

---
+++ Readme.md
@@ -0,0 +1,3 @@
+[![Build Status](https://travis-ci.org/haritzmedina/calculator.svg?branch=master)](https://travis-ci.org/haritzmedina/calculator)
+
+An example to test travis-ci and gradle. Maybe I will do the calculator in java.
--- src/test/java/com/haritzmedina/calculator/core/ArithmeticTest.java
+++ src/test/java/com/haritzmedina/calculator/core/ArithmeticTest.java
@@ -6,17 +6,18 @@ import static org.junit.Assert.*;
 
 /**
  * Created by Haritz Medina on 19/04/2016.
+ * Arithmetic class tests.
  */
 public class ArithmeticTest {
     @Test
     public void sum() throws Exception {
         assertEquals(new Double(4.0), Arithmetic.sum(1.5,2.5));
-        assertNotEquals(new Double(4.5), Arithmetic.sum(2.3, 2.1));
+        assertNotEquals(4.5, Arithmetic.sum(2.3, 2.1));
     }
 
     @Test
     public void subtract() throws Exception {
-
+        assertEquals(new Double(1.0), Arithmetic.subtract(2.0, 1.0));
     }
 
     @Test
